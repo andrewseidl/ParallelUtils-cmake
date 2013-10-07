236d00d221427c2080e31dae1179fa0555998693
Andrew Seidl <git@aas.io>
2013-10-06 22:29:36 -0500

    Cleaning up indentation

--- ParallelUtils.cmake
+++ ParallelUtils.cmake
@@ -28,18 +28,18 @@ set_property(CACHE CMAKE_BUILD_TYPE PROPERTY STRINGS "Release" "Debug" "RelWithD
 # Begin configuring MPI options
 macro(enable_mpi_support)
 
-		find_package("MPI" REQUIRED)
+    find_package("MPI" REQUIRED)
 
-		# Add the MPI-specific compiler and linker flags
-		# Also, search for #includes in MPI's paths
+    # Add the MPI-specific compiler and linker flags
+    # Also, search for #includes in MPI's paths
 
-        set(CMAKE_C_COMPILE_FLAGS "${CMAKE_C_COMPILE_FLAGS} ${MPI_C_COMPILE_FLAGS}")
-        set(CMAKE_C_LINK_FLAGS "${CMAKE_C_LINK_FLAGS} ${MPI_C_LINK_FLAGS}")
-		include_directories(${MPI_C_INCLUDE_PATH})
+    set(CMAKE_C_COMPILE_FLAGS "${CMAKE_C_COMPILE_FLAGS} ${MPI_C_COMPILE_FLAGS}")
+    set(CMAKE_C_LINK_FLAGS "${CMAKE_C_LINK_FLAGS} ${MPI_C_LINK_FLAGS}")
+    include_directories(${MPI_C_INCLUDE_PATH})
 
-        set(CMAKE_CXX_COMPILE_FLAGS "${CMAKE_CXX_COMPILE_FLAGS} ${MPI_CXX_COMPILE_FLAGS}")
-        set(CMAKE_CXX_LINK_FLAGS "${CMAKE_CXX_LINK_FLAGS} ${MPI_CXX_LINK_FLAGS}")
-		include_directories(${MPI_CXX_INCLUDE_PATH})
+    set(CMAKE_CXX_COMPILE_FLAGS "${CMAKE_CXX_COMPILE_FLAGS} ${MPI_CXX_COMPILE_FLAGS}")
+    set(CMAKE_CXX_LINK_FLAGS "${CMAKE_CXX_LINK_FLAGS} ${MPI_CXX_LINK_FLAGS}")
+    include_directories(${MPI_CXX_INCLUDE_PATH})
 
 endmacro(enable_mpi_support)
 # Done configuring MPI Options
@@ -56,11 +56,11 @@ endmacro(enable_mpi_support)
 # Begin configuring OpenMP options
 macro(enable_openmp_support)
 
-		find_package("OpenMP" REQUIRED)
+    find_package("OpenMP" REQUIRED)
 
-		# Add the OpenMP-specific compiler and linker flags
-        set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${OpenMP_CXX_FLAGS}")
-        set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${OpenMP_C_FLAGS}")
+    # Add the OpenMP-specific compiler and linker flags
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${OpenMP_CXX_FLAGS}")
+    set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${OpenMP_C_FLAGS}")
 
 endmacro(enable_openmp_support)
 # Done configuring OpenMP Options
@@ -78,72 +78,72 @@ endmacro(enable_openmp_support)
 # This is ugly...
 macro(enable_cuda_support)
 
-		# Hide a number of options from the default CMake screen
-		mark_as_advanced(CLEAR CUDA_BUILD_CUBIN)
-		mark_as_advanced(CLEAR CUDA_SDK_ROOT_DIR)
-		mark_as_advanced(CLEAR CUDA_TOOLKIT_ROOT_DIR)
-		mark_as_advanced(CLEAR CUDA_VERBOSE_BUILD)
-		mark_as_advanced(CLEAR CUDA_FAST_MATH)
-		mark_as_advanced(CLEAR CUDA_USE_CUSTOM_COMPILER)
-		mark_as_advanced(CLEAR CUDA_VERBOSE_PTX)
-		mark_as_advanced(CLEAR CUDA_DEVICE_VERSION)
+    # Hide a number of options from the default CMake screen
+    mark_as_advanced(CLEAR CUDA_BUILD_CUBIN)
+    mark_as_advanced(CLEAR CUDA_SDK_ROOT_DIR)
+    mark_as_advanced(CLEAR CUDA_TOOLKIT_ROOT_DIR)
+    mark_as_advanced(CLEAR CUDA_VERBOSE_BUILD)
+    mark_as_advanced(CLEAR CUDA_FAST_MATH)
+    mark_as_advanced(CLEAR CUDA_USE_CUSTOM_COMPILER)
+    mark_as_advanced(CLEAR CUDA_VERBOSE_PTX)
+    mark_as_advanced(CLEAR CUDA_DEVICE_VERSION)
 
-		# select Compute Capability
-		# This needs to be manually updated when devices with new CCs come out
-		set(CUDA_DEVICE_VERSION "20" CACHE STRING "CUDA Device Version")
-		set_property(CACHE CUDA_DEVICE_VERSION PROPERTY STRINGS "10" "11" "12" "13"	"20" "21" "30" "35")
+    # select Compute Capability
+    # This needs to be manually updated when devices with new CCs come out
+    set(CUDA_DEVICE_VERSION "20" CACHE STRING "CUDA Device Version")
+    set_property(CACHE CUDA_DEVICE_VERSION PROPERTY STRINGS "10" "11" "12" "13"	"20" "21" "30" "35")
 
-		# Enable fast-math for CUDA (_not_ GCC)
-		set(CUDA_FAST_MATH TRUE CACHE BOOL "Use Fast Math Operations")
+    # Enable fast-math for CUDA (_not_ GCC)
+    set(CUDA_FAST_MATH TRUE CACHE BOOL "Use Fast Math Operations")
 
-		# Tell nvcc to use a separate compiler for non-CUDA code.
-		# This is useful if you need to use an older of GCC than comes by default
-		set(CUDA_USE_CUSTOM_COMPILER FALSE CACHE BOOL "Use Custom Compiler")
-		set(CUDA_CUSTOM_COMPILER "" CACHE STRING "Custom C++ Compiler for CUDA If Needed")
+    # Tell nvcc to use a separate compiler for non-CUDA code.
+    # This is useful if you need to use an older of GCC than comes by default
+    set(CUDA_USE_CUSTOM_COMPILER FALSE CACHE BOOL "Use Custom Compiler")
+    set(CUDA_CUSTOM_COMPILER "" CACHE STRING "Custom C++ Compiler for CUDA If Needed")
 
-		# Shows register usage, etc
-		set(CUDA_VERBOSE_PTX TRUE CACHE BOOL "Show Verbose Kernel Info During Compilation")
+    # Shows register usage, etc
+    set(CUDA_VERBOSE_PTX TRUE CACHE BOOL "Show Verbose Kernel Info During Compilation")
 
 
-		# Let's get going...
-		find_package("CUDA" REQUIRED)
+    # Let's get going...
+    find_package("CUDA" REQUIRED)
 
-		# Frequently used in the examples
-		cuda_include_directories(${CUDA_SDK_ROOT_DIR}/common/inc)
-		cuda_include_directories(${CUDA_SDK_ROOT_DIR}/../shared/inc)
+    # Frequently used in the examples
+    cuda_include_directories(${CUDA_SDK_ROOT_DIR}/common/inc)
+    cuda_include_directories(${CUDA_SDK_ROOT_DIR}/../shared/inc)
 
-		set(CUDA_SDK_LIB_DIR ${CUDA_SDK_ROOT_DIR}/common/lib
-				${CUDA_SDK_ROOT_DIR}/lib ${CUDA_SDK_ROOT_DIR}/../shared/lib)
+    set(CUDA_SDK_LIB_DIR ${CUDA_SDK_ROOT_DIR}/common/lib
+        ${CUDA_SDK_ROOT_DIR}/lib ${CUDA_SDK_ROOT_DIR}/../shared/lib)
 
-		# Set custom compiler flags
-		set(CUDA_NVCC_FLAGS "" CACHE STRING "" FORCE)
+    # Set custom compiler flags
+    set(CUDA_NVCC_FLAGS "" CACHE STRING "" FORCE)
 
-		if(CUDA_USE_CUSTOM_COMPILER)
-				mark_as_advanced(CLEAR CUDA_CUSTOM_COMPILER)
-				list(APPEND CUDA_NVCC_FLAGS "-ccbin=${CUDA_CUSTOM_COMPILER}")
-		else()
-				mark_as_advanced(FORCE CUDA_CUSTOM_COMPILER)
-		endif()
+    if(CUDA_USE_CUSTOM_COMPILER)
+        mark_as_advanced(CLEAR CUDA_CUSTOM_COMPILER)
+        list(APPEND CUDA_NVCC_FLAGS "-ccbin=${CUDA_CUSTOM_COMPILER}")
+    else()
+        mark_as_advanced(FORCE CUDA_CUSTOM_COMPILER)
+    endif()
 
-		# Macro for setting the Compute Capability
-		macro(set_compute_capability cc)
-				list(APPEND CUDA_NVCC_FLAGS "-gencode=arch=compute_${cc},code=sm_${cc}")
-				list(APPEND CUDA_NVCC_FLAGS "-gencode=arch=compute_${cc},code=compute_${cc}")
-		endmacro(set_compute_capability)
+    # Macro for setting the Compute Capability
+    macro(set_compute_capability cc)
+        list(APPEND CUDA_NVCC_FLAGS "-gencode=arch=compute_${cc},code=sm_${cc}")
+        list(APPEND CUDA_NVCC_FLAGS "-gencode=arch=compute_${cc},code=compute_${cc}")
+    endmacro(set_compute_capability)
 
-		# Tell nvcc to compile for the selected Compute Capability
-		# This can also be called from the main CMakeLists.txt to enable
-		# support for additional CCs
-		set_compute_capability(${CUDA_DEVICE_VERSION})
+    # Tell nvcc to compile for the selected Compute Capability
+    # This can also be called from the main CMakeLists.txt to enable
+    # support for additional CCs
+    set_compute_capability(${CUDA_DEVICE_VERSION})
 
-		# Enable fast-math if selected
-		if(CUDA_FAST_MATH)
-				list(APPEND CUDA_NVCC_FLAGS "-use_fast_math")
-		endif()
+    # Enable fast-math if selected
+    if(CUDA_FAST_MATH)
+        list(APPEND CUDA_NVCC_FLAGS "-use_fast_math")
+    endif()
 
-		# Enable verbose compile if selected
-		if(CUDA_VERBOSE_PTX)
-				list(APPEND CUDA_NVCC_FLAGS "--ptxas-options=-v")
-		endif()
+    # Enable verbose compile if selected
+    if(CUDA_VERBOSE_PTX)
+        list(APPEND CUDA_NVCC_FLAGS "--ptxas-options=-v")
+    endif()
 endmacro(enable_cuda_support)
 # Done configuring CUDA options
